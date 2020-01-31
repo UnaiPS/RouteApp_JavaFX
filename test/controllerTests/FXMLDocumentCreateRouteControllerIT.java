ae183ad513c75d6f3741175871902a69b58228a6
Jon Calvo <jon.clavo@reto1.com>
2020-01-31 08:46:06 +0100

    Added Daira Test

--- test/controllerTests/FXMLDocumentCreateRouteControllerIT.java
+++ test/controllerTests/FXMLDocumentCreateRouteControllerIT.java
@@ -1,23 +1,36 @@
 package controllerTests;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
+import model.User;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+import org.loadui.testfx.controls.TextInputControls;
+import org.testfx.api.FxAssert;
 import static org.testfx.api.FxAssert.verifyThat;
 import org.testfx.framework.junit.ApplicationTest;
 import static org.testfx.matcher.base.NodeMatchers.isVisible;
-//import routeapp_javafx.RouteApp_JavaFX;
+import org.testfx.matcher.control.TableViewMatchers;
+import org.testfx.robot.Motion;
+import view.FXMLDocumentControllerLogin;
+import view.FXMLDocumentCreateRouteController;
 
 /**
  * The integration test for the FXMLDocumentCreateRouteController.
+ *
  * @author Daira Eguzkiza
  */
-
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class FXMLDocumentCreateRouteControllerIT extends ApplicationTest{
-    private static final String INVALIDROUTE="iaugduaysgvruidwt42836trqigw";
-    private static final String LONGROUTENAME="dkufbiuapbdfiabfvibfvivfirfbie"
+public class FXMLDocumentCreateRouteControllerIT extends ApplicationTest {
+
+    private static boolean firstTime = true;
+    private static final String USER = "Jon";
+    private static final String PASSWORD = "abcd*1234";
+    private static final String INVALIDROUTE = "iaugduaysgvruidwt42836trqigw";
+    private static final String LONGROUTENAME = "dkufbiuapbdfiabfvibfvivfirfbie"
             + "aaasdiufbaoisdbiaoslbdfaisdfaa"
             + "duhfaildfhgioadgbiladfbviadbvi"
             + "dkhjbfliadfbipadbgadfguabgalid"
@@ -26,16 +39,36 @@ public class FXMLDocumentCreateRouteControllerIT extends ApplicationTest{
             + "dljfngsdikfbnskdfbiñsfbñsidfif"
             + "dfgkjbdfibdfiabdfidbfsdkfbsdkf"
             + "dfkjnsdifbsklfdbjnslkfbjksfdjf";
-    private static final String VALIDROUTE="bilbo";
-    
-    
- 
-    @Override public void start(Stage stage) throws Exception {
-      //new RouteApp_JavaFX().start(stage);
+    private static final String VALIDROUTE = "bilbo";
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        if (firstTime) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/LogIn.fxml"));
+            Parent root = (Parent) loader.load();
+            FXMLDocumentControllerLogin viewController = loader.getController();
+            viewController.setStage(stage);
+            viewController.initStage(root);
+            firstTime = false;
+        }
+
     }
-    
+
     /**
-     * Tests if the program won't let the user save a route if they haven't 
+     * First we go to the Create Route window.
+     */
+    @Test
+    public void testAA_GoToCreateRoute() {
+        clickOn("#txtLogin");
+        write(USER);
+        clickOn("#txtPassword");
+        write(PASSWORD);
+        clickOn("#btnLogin");
+        clickOn("#btnCreateRoute");
+    }
+
+    /**
+     * Tests if the program won't let the user save a route if they haven't
      * filled all the fields.
      */
     @Test
@@ -51,7 +84,7 @@ public class FXMLDocumentCreateRouteControllerIT extends ApplicationTest{
         clickOn("#tfOrigin");
         write(VALIDROUTE);
         clickOn("#btnCheckOrigin");
-        verifyThat("Is this the direction you want?: Bilbo, Euskadi, Espainia", isVisible());
+        verifyThat("Is this the direction you want?: Bilbao, Basque Country, Spain", isVisible());
         clickOn("Aceptar");
         clickOn("#btnSaveRoute");
         verifyThat("You must enter at least one destination.", isVisible());
@@ -59,7 +92,7 @@ public class FXMLDocumentCreateRouteControllerIT extends ApplicationTest{
         clickOn("#tfDestination");
         write(VALIDROUTE);
         clickOn("#btnCheckDestination");
-        verifyThat("Is this the direction you want?: Bilbo, Euskadi, Espainia", isVisible());
+        verifyThat("Is this the direction you want?: Bilbao, Basque Country, Spain", isVisible());
         clickOn("Aceptar");
         clickOn("#btnSaveRoute");
         verifyThat("You must enter a route mode.", isVisible());
@@ -71,11 +104,11 @@ public class FXMLDocumentCreateRouteControllerIT extends ApplicationTest{
         clickOn("#rdbtnCar");
         clickOn("#btnSaveRoute");
         verifyThat("You must enter someone to make the delivery. If you "
-                                    + "don't want to do this now, please"
-                                    + "check the \"Don't assign yet\" option.", isVisible());
+                + "don't want to do this now, please"
+                + "check the \"Don't assign yet\" option.", isVisible());
         clickOn("Aceptar");
     }
-    
+
     /**
      * Tests that the app will show an alert if a user tries to enter a route
      * name that is too long.
@@ -88,55 +121,75 @@ public class FXMLDocumentCreateRouteControllerIT extends ApplicationTest{
         verifyThat("The name you've entered is too long", isVisible());
         clickOn("Aceptar");
     }
-    
+
     /**
-     * Tests if the app will show an alert if the user wrote an invalid address on the
-     * origin field.
+     * Tests if the app will show an alert if the user wrote an invalid address
+     * on the origin field.
      */
     @Test
     public void testBInvalidOriginRoute() {
         clickOn("#tfOrigin");
         write(INVALIDROUTE);
         clickOn("#btnCheckOrigin");
-        verifyThat("We couldn't find any direction with the entered data.", isVisible());
+        verifyThat("We can't load data right now. Please try again later.", isVisible());
         clickOn("Aceptar");
     }
-    
+
     /**
-     * Tests if the app will show an alert if the user wrote an invalid address on the
-     * destinations field.
+     * Tests if the app will show an alert if the user wrote an invalid address
+     * on the destinations field.
      */
     @Test
-    public void testCInvalidDestinationRoute() {
+    public void testBInvalidDestinationRoute() {
         clickOn("#tfDestination");
         write(INVALIDROUTE);
         clickOn("#btnCheckDestination");
-        verifyThat("We couldn't find any direction with the entered data.", isVisible());
+        verifyThat("We can't load data right now. Please try again later.", isVisible());
         clickOn("Aceptar");
     }
-    
+
     /**
-     * Tests that the app will show the user the direction the external web api has found.
+     * Tests that the app will show the user the direction the external web api
+     * has found.
      */
     @Test
-    public void testBValidOriginRoute() {
+    public void testCAValidOriginRoute() {
         clickOn("#tfOrigin");
+        TextInputControls.clearTextIn("#tfOrigin");
         write(VALIDROUTE);
         clickOn("#btnCheckOrigin");
-        verifyThat("Is this the direction you want?: Bilbo, Euskadi, Espainia", isVisible());
+        verifyThat("Is this the direction you want?: Bilbao, Basque Country, Spain", isVisible());
         clickOn("Aceptar");
+    }
+
+    
+
+    /**
+     * Tests that the app will show the user the direction the external web api
+     * has found.
+     */
+    @Test
+    public void testCBTableShowingDest() {
+        clickOn("#tfDestination");
+        TextInputControls.clearTextIn("#tfDestination");
+        write(VALIDROUTE);
+        clickOn("#btnCheckDestination");
+        verifyThat("Is this the direction you want?: Bilbao, Basque Country, Spain", isVisible());
+        clickOn("Aceptar");
+        FxAssert.verifyThat("#tvDestinations", TableViewMatchers.containsRow("Bilbao, Basque Country, Spain"));
+
     }
     
     /**
-     * Tests that the app will show the user the direction the external web api has found.
+     * Tests that the app will show the user the direction the external web api
+     * has found.
      */
     @Test
-    public void testCValidDestinationRoute() {
-        clickOn("#tfDestination");
-        write(VALIDROUTE);
-        clickOn("#btnCheckDestination");
-        verifyThat("Is this the direction you want?: Bilbo, Euskadi, Espainia", isVisible());
-        clickOn("Aceptar");
+    public void testCCTableContextMenu() {
+        moveTo("#tvDestinations");
+        moveBy(-300, -160, Motion.DIRECT);
+        rightClickOn();
+        clickOn("Delete Row");
+        FxAssert.verifyThat("#tvDestinations", TableViewMatchers.hasNumRows(1));
     }
-    
 }
